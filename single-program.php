bb480231e8021530122f705d04544bff10c93de2
Rezwan <rezwan.storerepublic@gmail.com>
2019-07-29 16:52:37 +0600

    Professor Menu Added

--- single-program.php
+++ single-program.php
@@ -19,8 +19,41 @@
           <?php the_content( ); ?>
 </div>
 
-
+<!-- Related Faculty !-->
 <?php
+         $relatedprofessor =new WP_Query(array(
+          'posts_per_page'=> -1,
+          'post_type' => 'professor',
+          'orderby'=> 'title',
+          'order' => 'ASC',
+          'meta_query' => array(
+           array(
+                'key' => "related_programs",
+                'compare' => 'LIKE',
+                'value' => '"'. get_the_ID(  ).'"'
+
+            )
+          )
+        ));
+        if($relatedprofessor->have_posts()) {
+
+        
+
+        echo '<hr class="section-break">';
+        echo '<h2 class="headline headline--medium">'.get_the_title().' Professors</h2>';
+          while($relatedprofessor -> have_posts()){
+            $relatedprofessor -> the_post();?>
+           
+           <li><a href="<?php the_permalink( ); ?>"><?php the_title();?></a></li>
+        
+        <?php  }
+        
+    } 
+
+ wp_reset_postdata(  ); 
+
+
+
         $today = date('Ymd');
         $homepageEvents =new WP_Query(array(
           'posts_per_page'=> 2,
